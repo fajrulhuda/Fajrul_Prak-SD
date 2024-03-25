b04868fdf9b65fbb96dab5ce805336281a0936d6
Fajrul Huda Ash Shiddiq <144108509+fajrulhuda@users.noreply.github.com>
2024-03-25 18:07:13 +0700

    Add files via upload

---
+++ Jobsheet 2/Latihan/latihan 2.7.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+typedef struct Mahasiswa {
+	char nama[50];
+	char jurusan[50];
+	int nilai;
+}data;
+
+int main(void){
+	
+	data mhs01;
+	
+	printf("Masukkan nama mahasiswa: ");
+	scanf("%s", &mhs01.nama);
+	printf("Masukkan jurusan: ");
+	scanf("%s", &mhs01.jurusan);
+	mhs01.nilai = 100;
+	
+	printf("%s adalah mahasiswa jurusan %s\n", &mhs01.nama, &mhs01.jurusan);
+	printf("memperoleh nilai UAS %i \n", mhs01.nilai);
+	
+	return 0;
+}
